ee1dbd04cbb29c6bb42496c7a829acc687ebcc75
Federico Córdoba <79395978+mercuryeater@users.noreply.github.com>
2023-06-07 02:55:04 -0500

    feat: :sparkles: add-volta (#4)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,10 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: "eslint:recommended",
+  extends: 'eslint:recommended',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {},
 };
