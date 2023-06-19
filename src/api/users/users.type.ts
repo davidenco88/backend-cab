cd8d0c6cc5c417365703e8ef61f8e09ff68e7e6d
davidenco88 <122131492+davidenco88@users.noreply.github.com>
2023-06-19 22:44:14 +0200

    feat: middlewares-roles-validation (#24)

--- src/api/users/users.type.ts
+++ src/api/users/users.type.ts
@@ -4,14 +4,27 @@ import { Users as UserModel, Rol as RoleModel } from '@prisma/client';
 export type usersCreateData = Users;
 
 export type User = UserModel;
-export interface UserWithRoles extends UserModel {
-  roles: RoleModel[];
-}
+
+export type UserWithRoles = {
+  id: number;
+  name: string;
+  lastname: string;
+  email: string;
+  avatar: string;
+  isActive: boolean;
+  password: string;
+  UserByRole: {
+    Rol: {
+      id: number;
+      name: string;
+    };
+  }[];
+};
+
 export type updateUser = {
   id: number;
   name: string;
   email: string;
   avatar: string;
   rol_id: number;
-}
-
+};
