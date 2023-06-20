7f808a0a7f63759eedec97fbdb2d3a9c8f5fd0c4
Federico <federicoc03@gmail.com>
2023-06-19 19:35:50 -0500

    feat: :sparkles: add-middlewares-to-userApi

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -6,6 +6,7 @@ import {
   updateUserHandler,
   deleteUserHandler,
 } from './users.controller'
+import { middlewareHasRol, middlewareRolAdmin } from "../../auth/auth.controller";
 
 const userRouter = Router();
 
@@ -13,16 +14,16 @@ const userRouter = Router();
 userRouter.post('/', createUserHandler);
 
 // /api/users --> GET
-userRouter.get('/', getAllUserHandler);
+userRouter.get('/', middlewareRolAdmin , getAllUserHandler);
 
 // /api/users/:id --> GET
-userRouter.get('/:id', getUserByIdHandler);
+userRouter.get('/:id', middlewareHasRol , getUserByIdHandler);
 
-// /api/users --> PATHC
-userRouter.patch('/:id', updateUserHandler);
+// /api/users --> PATCH
+userRouter.patch('/:id', middlewareHasRol , updateUserHandler);
 
 // /api/users --> DELETE
-userRouter.patch('/delete/:id', deleteUserHandler);
+userRouter.patch('/delete/:id', middlewareRolAdmin , deleteUserHandler);
 
 
 export default userRouter;
