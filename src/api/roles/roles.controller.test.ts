37409e15af378fbaf350e3db33a0a7a18be451c6
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2023-06-23 21:20:01 -0500

    feat: tesrteo de roles get y get by id

--- src/api/roles/roles.controller.test.ts
+++ src/api/roles/roles.controller.test.ts
@@ -35,3 +35,45 @@ describe('Useres controller', () => {
     });
   });
 });
+
+describe('Roles getRolById test', () => {
+  describe('GET /api/roles/:id', () => {
+    test('should respond a 200 status code', async () => {
+      // Arrange
+      const statusCode = 200;
+      //const taskId = 1;
+      const expected =  {
+        id: 1,
+        name: "Admin",
+        isActive: true
+       };
+      const expected2 = {
+        id: 2,
+        name: "Client",
+        isActive: true
+      };
+      const expected3 = {
+        id: 3,
+        name: "Driver",
+        isActive: true
+      };
+
+      // Act
+      const res  = await request.get('/api/roles/1');
+      const res2 = await request.get('/api/roles/2');
+      const res3 = await request.get('/api/roles/3');
+      // Assert
+      expect(res.status).toBe(statusCode);
+      expect(res).not.toBeNull();
+      expect(res.body).toEqual(expected);
+
+      expect(res2.status).toBe(statusCode);
+      expect(res2).not.toBeNull();
+      expect(res2.body).toEqual(expected2);
+
+      expect(res3.status).toBe(statusCode);
+      expect(res3).not.toBeNull();
+      expect(res3.body).toEqual(expected3);
+    });
+  });
+});
