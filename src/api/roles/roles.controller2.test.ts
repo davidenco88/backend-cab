efc4ecdcdcb5440bf0112f328e6649c2bced46b4
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2023-06-24 09:54:23 -0500

    build: cambios estructura test roles

--- src/api/roles/roles.controller2.test.ts
+++ src/api/roles/roles.controller2.test.ts
@@ -3,7 +3,7 @@ import { app } from "../../app" ;
 
 const request = supertest(app);
 
-describe('Useres controller', () => {
+/*describe('Useres controller', () => {
   describe('GET /api/roles', () => {
     test('should respond a 200 status code', async () => {
       // Arrange
@@ -34,20 +34,25 @@ describe('Useres controller', () => {
       expect(res.body).toEqual(response);
     });
   });
-});
-/*
+});*/
+
 describe('Roles getRolById test', () => {
-  describe('GET /api/roles/:id', () => {
+  describe('GET /api/roles/:1', () => {
     test('should respond a 200 status code', async () => {
       // Arrange
       const statusCode = 200;
       //const taskId = 1;
       const expected =  {
-        id: 1,
-        name: "Admin",
-        isActive: true
+        "id": 1,
+        "name": "Admin",
+        "isActive": true
        };
-      const expected2 = {
+
+
+      // Act
+      const res  = await request.get('/api/roles/1');
+
+      /*const expected2 = {
         id: 2,
         name: "Client",
         isActive: true
@@ -57,23 +62,22 @@ describe('Roles getRolById test', () => {
         name: "Driver",
         isActive: true
       };
-
-      // Act
-      const res  = await request.get('/api/roles/:1');
-      const res2 = await request.get('/api/roles/:2');
-      const res3 = await request.get('/api/roles/:3');
-      // Assert
-      expect(res.status).toBe(statusCode);
-      expect(res).not.toBeNull();
-      expect(res.body).toEqual(expected);
-
+      const res2 = await request.get('/api/roles/2');
+      const res3 = await request.get('/api/roles/3');
       expect(res2.status).toBe(statusCode);
       expect(res2).not.toBeNull();
       expect(res2.body).toEqual(expected2);
 
       expect(res3.status).toBe(statusCode);
       expect(res3).not.toBeNull();
-      expect(res3.body).toEqual(expected3);
+      expect(res3.body).toEqual(expected3);*/
+      // Assert
+      console.log(res.status)
+      expect(res.status).toBe(statusCode);
+     /* expect(res).not.toBeNull();
+      expect(res.body).toEqual(expected);*/
+
+
     });
   });
-});*/
+});
