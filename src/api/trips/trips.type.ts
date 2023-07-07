09e183d9e147a4907a1694fcc1f9f69f31a45041
Federico Córdoba <79395978+mercuryeater@users.noreply.github.com>
2023-07-07 12:23:01 -0500

    feat: :sparkles: calculated-Trip-delivered (#61)

--- src/api/trips/trips.type.ts
+++ src/api/trips/trips.type.ts
@@ -1,5 +1,4 @@
-import { Trips } from '@prisma/client';
-import sgMail from '@sendgrid/mail';
+import { Trips, VehicleTypes } from '@prisma/client';
 export type tripsCreateData = Trips;
 
 export type tripsEmailCreatedData ={
@@ -11,5 +10,52 @@ export type tripsEmailCreatedData ={
   totalPrice  :      Number,
   pickUpDate :       Date,
   toEmail :          String,
-
 }
+
+type User = {
+  id: number;
+  name: string;
+  lastname: string;
+  avatar: string;
+}
+type VehicleType = VehicleTypes;
+type SelectedVehicle = {
+  id: number;
+  driverID: number;
+  vehicleTypeID: number;
+  image: string;
+  brand: string;
+  model: string;
+  year: number;
+  plates: string;
+  isAvailable: boolean;
+  isActive: boolean;
+  Users: User;
+  VehicleTypes: VehicleType;
+}
+
+export type CreateTripType = {
+  id?: number;
+  pickUpLatitude:    string;
+  pickUpLongitude:   string;
+  dropOffLatitude:  string;
+  dropOffLongitude: string;
+  pickUpDate: string;
+  selectedVehicle:  SelectedVehicle;
+}
+
+export type CreateTripTypeCalculated = {
+  id?: number;
+  pickUpLatitude: string;
+  pickUpLongitude: string;
+  dropOffLatitude: string;
+  dropOffLongitude: string;
+  clientID: number;
+  vehicleID: number;
+  travelDistance: number;
+  totalPrice: number;
+  pickUpDate: Date;
+  isActive: boolean;
+}
+
+
